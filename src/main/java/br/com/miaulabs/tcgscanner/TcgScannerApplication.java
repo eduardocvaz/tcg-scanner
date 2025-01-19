f69b13a9b60cff2253eea2c8778ab577d430a0f8
eduardocvaz <eduardo.c.vaz@hotmail.com>
2025-01-19 14:50:19 -0300

    Init

---
+++ src/main/java/br/com/miaulabs/tcgscanner/TcgScannerApplication.java
@@ -0,0 +1,13 @@
+package br.com.miaulabs.tcgscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcgScannerApplication.class, args);
+    }
+
+}
